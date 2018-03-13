3764a97b90d20e20135ea76a0ee0df35b45e7444
Ju111 <SchneiderJu@gmx.net>
2018-03-13 12:20:12 +0100

    GET page from database

--- src/pages/controller.ts
+++ src/pages/controller.ts
@@ -1,43 +1,59 @@
 import { JsonController, Get, Param, Put, Body, Post, HttpCode } from 'routing-controllers'
-import pagesById, { Page } from './data'
-import { PageList } from './data'
+import Page from './entity'
 
 @JsonController()
 export default class PageController {
 
-  @Get('/pages')
-   allPages():
-    PageList {
-      return {
-        pages: Object.values(pagesById)
-      }
+  @Get('/pages/:id')
+  getPage(
+    @Param('id') id: number
+  ) {
+      return Page.findOneById(id)
     }
 
-    @Get('/pages/:id')
-    getPage(
-      @Param('id') id: number
-    ):
-      Page {
-        return pagesById[id]
-      }
-
-    @Put('/pages/:id')
-      updatePage(
-        @Param('id') id: number,
-        @Body() body: Partial<Page>
-      ):
-        Page {
-          console.log(`Incoming PUT body param:`, body)
-          return pagesById[id]
-        }
-
-    @Post('/pages')
-      @HttpCode(201)
-      createPage(
-        @Body() body: Page
-      ):
-        Page {
-          console.log(`Incoming POST body param:`, body)
-          return body
-        }
+  @Get('/pages')
+    async allPages() {
+      const pages = await Page.find()
+      return { pages }
+    }
 }
+
+//
+//
+
+//   @Get('/pages')
+//    allPages():
+//     PageList {
+//       return {
+//         pages: Object.values(pagesById)
+//       }
+//     }
+//
+//     @Get('/pages/:id')
+//     getPage(
+//       @Param('id') id: number
+//     ):
+//       Page {
+//         return pagesById[id]
+//       }
+//
+//     @Put('/pages/:id')
+//       updatePage(
+//         @Param('id') id: number,
+//         @Body() body: Partial<Page>
+//       ):
+//         Page {
+//           console.log(`Incoming PUT body param:`, body)
+//           return pagesById[id]
+//         }
+//
+//     @Post('/pages')
+//       @HttpCode(201)
+//       createPage(
+//         @Body() body: Page
+//       ):
+//         Page {
+//           console.log(`Incoming POST body param:`, body)
+//           return body
+//         }
+// }
