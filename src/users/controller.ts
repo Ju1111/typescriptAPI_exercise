17966c900e4ee547dc969ee9ab81aa4b294c6b24
Ju111 <SchneiderJu@gmx.net>
2018-03-13 13:19:25 +0100

    users target added including all endpoints

---
+++ src/users/controller.ts
@@ -0,0 +1,38 @@
+import { JsonController, Get, Param, Put, Body, NotFoundError, Post, HttpCode } from 'routing-controllers'
+import User from './entity'
+
+@JsonController()
+export default class UserController {
+
+  @Get('/users/:id')
+  getPage(
+    @Param('id') id: number
+  ) {
+      return User.findOneById(id)
+    }
+
+  @Get('/users')
+    async allUsers() {
+      const users = await User.find()
+      return { users }
+    }
+
+  @Put('/users/:id')
+    async updatePage(
+      @Param('id') id: number,
+      @Body() update: Partial<User>
+    ) {
+        const user = await User.findOneById(id)
+        if (!user) throw new NotFoundError('Cannot find user')
+
+        return User.merge(user, update).save()
+      }
+
+    @Post('/users')
+    @HttpCode(201)
+    createUser(
+      @Body() user: User
+    ) {
+        return user.save()
+      }
+}
